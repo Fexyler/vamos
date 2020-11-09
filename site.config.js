6b1ed78570187a47a55e048357375c1f4a87e8ad
omeraksu <omeeraksu@gmail.com>
2020-11-09 12:31:41 +0300

    start config

---
+++ site.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  title: 'Vamos!',
+  description: 'Vamos! landing page.',
+  siteUrl: 'https://vamos.ist',
+  googleAnalytic: 'X-XXXXXXXXXX',
+  author: {
+    email: 'mail@gmail.com',
+    name: 'Recep Yılancıoğlu'
+  },
+  lang: 'tr'
+}
