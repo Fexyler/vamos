33bf41e728ddf601d4dcb5a267fff28adf7e4922
Fexyler <30533238+Fexyler@users.noreply.github.com>
2020-12-17 23:07:09 +0300

    api token reveal 🎃
    lol

--- next.config.js
+++ next.config.js
@@ -7,7 +7,7 @@ const nextConfig = {
   env: {
     API_URL: 'https://vamosreal.herokuapp.com/api/contact',
     API_TOKEN:
-      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzZWNvbmRTZWNyZXQiOiJhNWM5ZThjMS03ODc3LTQ1ZWItOTk5MC01YmQ4ZjcyZjA3MWUiLCJpYXQiOjE2MDUzNTM0MDJ9.I-3mcjSP3rcVUY4LdfjHpntOZpXc_ZcIWFRN2X1fx7g'
+      ''
   },
   pwa: {
     dest: 'public',
